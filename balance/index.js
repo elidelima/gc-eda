84bce78bb8f8a21f6a01cfe9597f015b1fea595e
Eli de Lima <elimigueldelima@gmail.com>
2024-05-07 20:26:37 +0100

    feat: adding consumer for balance topic

--- balance/index.js
+++ balance/index.js
@@ -5,9 +5,9 @@ const app = express();
 const port = 3003;
 
 const dbconfig = {
-    host: 'localhost',
+    host: 'balancedb',
+    port: 3306,
     user: 'root',
-    port: 3307,
     password: 'root',
     database: 'balance',
 };
@@ -68,8 +68,8 @@ app.get('/balances/:account_id', async (req, res) => {
             if (error) return res.send('Internal Server Error');
 
             if (!result || result.length === 0) return res.status(404).send('404 - Not found');
-            
-            return res.json( result[0]);
+
+            return res.json(result[0]);
         });
 
     } catch (error) {
@@ -82,3 +82,73 @@ app.get('/balances/:account_id', async (req, res) => {
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 });
+
+// TOPIC CONSUMER
+const { Kafka } = require('kafkajs');
+
+const kafka = new Kafka({
+    clientId: 'balanceapp',
+    brokers: ['kafka:29092'],
+});
+
+async function updateBalances(payload) {
+    const {
+        account_id_from,
+        balance_account_id_from,
+        account_id_to,
+        balance_account_id_to,
+    } = payload;
+    connection = mysql.createConnection(dbconfig);
+    connection.beginTransaction(function (err) {
+        if (err) { throw err; }
+        //UPDATE balance account_from
+        connection.query(
+            'UPDATE balances SET balance = ? WHERE account_id = ?',
+            [balance_account_id_from, account_id_from],
+            function (error, results) {
+                if (error) {
+                    return connection.rollback(function () {
+                        throw error;
+                    });
+                }
+
+                //UPDATE balance account_to
+                connection.query(
+                    'UPDATE balances SET balance = ? WHERE account_id = ?',
+                    [balance_account_id_to, account_id_to],
+                    function (error, results) {
+                        if (error) {
+                            return connection.rollback(function () {
+                                throw error;
+                            });
+                        }
+                        connection.commit(function (err) {
+                            if (err) {
+                                return connection.rollback(function () {
+                                    throw err;
+                                });
+                            }
+                            console.log(`Balances updated successfully!`);
+                        });
+                    });
+            });
+    });
+}
+
+const subscribe = async () => {
+    const consumer = kafka.consumer({ groupId: 'wallet' })
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'balances', fromBeginning: true })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+            const { Payload: payload } = JSON.parse(message.value);
+            console.log({ prefix, payload });
+            
+            await updateBalances(payload);
+        },
+    });
+}
+
+subscribe().catch((e) => console.error(`[wallet/balances] - ${e}`));
